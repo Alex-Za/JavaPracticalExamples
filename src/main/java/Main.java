0a169d3ee8bbd8c25758da4a15f877522a6c64bc
Aleksandr Zaitsev <argiluckone@gmail.com>
2021-11-22 23:48:53 +0200

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 import multithreading.callable.CallableDemo;
 import multithreading.count_down_latch.CDLDemo;
 import multithreading.cyclic_barrier.CyclicBarrierDemo;
+import multithreading.fork_join.ForkJoinDemo;
 import multithreading.locker.LockDemo;
 import multithreading.phaser.PhaserDemo;
 
 public class Main {
     public static void main(String[] args) {
 
-        LockDemo lockDemo = new LockDemo();
-        lockDemo.run();
+        ForkJoinDemo forkJoinDemo = new ForkJoinDemo();
+        forkJoinDemo.run();
 
     }
 }
---
+++ src/main/java/multithreading/fork_join/ForkJoinDemo.java
@@ -0,0 +1,32 @@
+package multithreading.fork_join;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinDemo {
+    public void run() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        double[] nums = new double[100000];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (double) i;
+        }
+
+        System.out.println("Part of original sequence");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nums[i] + " ");
+        }
+        System.out.println("\n");
+
+        SqrtTransform task = new SqrtTransform(nums, 0, nums.length);
+
+        forkJoinPool.invoke(task);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.format("%.4f ", nums[i]);
+        }
+        System.out.println();
+
+    }
+}
